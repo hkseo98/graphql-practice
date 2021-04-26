d5c56d5f17bd36311ae7c4b02552ec2a879414e5
hkseo98 <69430175+hkseo98@users.noreply.github.com>
2021-04-27 00:12:09 +0900

    graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,23 @@
+
 module.exports = {
   siteMetadata: {
-    title: "My Gatsby Site",
+    title: `Pandas Eating Lots`
   },
-  plugins: [],
-};
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-plugin-emotion`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
+}
